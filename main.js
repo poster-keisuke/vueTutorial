f80605ffabd26d75b5d64168b5f5eb9769de4333
poster-keisuke <ta.keisuke4@gmail.com>
2020-03-12 10:15:19 +0900

    add: until lesson 6

--- main.js
+++ main.js
@@ -3,6 +3,33 @@ var app = new Vue({
     data: {
         product: 'Socks',
         image: './assets/vmSocks-green.jpg',
-        altText: 'A pair of socks'
+        altText: 'A pair of socks',
+        inStock: false,
+        details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+        variants: [{
+                variantsId: 2234,
+                variantColor: 'green',
+                variantImage: './assets/vmSocks-green.jpg'
+            },
+            {
+                variantsId: 2235,
+                variantColor: 'blue',
+                variantImage: './assets/vmSocks-blue.jpg'
+            }
+        ],
+        cart: 0
+    },
+    methods: {
+        addToCart() {
+            this.cart++
+        },
+        deleteFromCart() {
+            if (this.cart > 0) {
+                this.cart--
+            }
+        },
+        updateProduct(variantImage) {
+            this.image = variantImage
+        }
     }
 })
