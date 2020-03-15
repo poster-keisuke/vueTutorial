9311f5e82f60eebd10ec98e4376b76823fbd3d4c
poster-keisuke <ta.keisuke4@gmail.com>
2020-03-16 00:25:52 +0900

    add: introduce axios for api responce

--- RealWorldVue/README.md
+++ RealWorldVue/README.md
@@ -1,24 +1,35 @@
 # real-world-vue
 
 ## Project setup
+
 ```
 npm install
 ```
 
 ### Compiles and hot-reloads for development
+
 ```
 npm run serve
 ```
 
 ### Compiles and minifies for production
+
 ```
 npm run build
 ```
 
 ### Lints and fixes files
+
 ```
 npm run lint
 ```
 
 ### Customize configuration
+
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+### Build Json Server
+
+```
+npx json-server --watch db.json
+```
--- RealWorldVue/src/components/EventCard.vue
+++ RealWorldVue/src/components/EventCard.vue
@@ -1,32 +1,20 @@
 <template>
   <router-link
     class="event-link"
-    :to="{ name: 'event-show', params: { id: '1' } }"
+    :to="{ name: 'event-show', params: { id: event.id } }"
   >
     <div class="event-card -shadow">
       <span class="eyebrow">@{{ event.time }} on {{ event.date }}</span>
       <h4 class="title">{{ event.title }}</h4>
-      <BaseIcon name="users" />
-      <span>{{ event.attendees.length }} attending</span>
+      <BaseIcon name="users">{{ event.attendees.length }} attending</BaseIcon>
     </div>
   </router-link>
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-      event: {
-        id: 1,
-        title: 'Beach Clieanup',
-        date: 'Tues Aug 19, 2020',
-        time: '6:00',
-        attendees: [
-          { id: 'abc123', name: 'Adam Jahar' },
-          { id: 'def456', name: 'Keisuke taa' }
-        ]
-      }
-    }
+  props: {
+    event: Object
   }
 }
 </script>
